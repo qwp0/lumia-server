6a9976e4c146e54a7ed47ba15bed79882c55066f
Soyoung Park <soyoung008@naver.com>
2025-06-28 01:31:32 +0900

    chore: dotenv 설정 및 cors, express.json() 미들웨어 적용

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+
+app.use(cors());
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello from Lumia Server!");
