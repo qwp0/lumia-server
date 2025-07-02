0bb8aea8dca612933c417a8cdb66fae958c67a99
Soyoung Park <soyoung008@naver.com>
2025-07-02 16:18:15 +0900

    feat: 슬라이드 발표방 생성 API 구현 (POST /room/create) #4

---
+++ routes/room.js
@@ -0,0 +1,28 @@
+import express from "express";
+import { roomStore } from "../stores/roomStore.js";
+import { v4 as uuidv4 } from "uuid";
+
+const roomRouter = express.Router();
+
+roomRouter.post("/create", (req, res) => {
+  const { slideUrl } = req.body;
+
+  if (!slideUrl) {
+    return res.status(400).json({ error: "slideUrl이 필요합니다." });
+  }
+
+  const roomId = uuidv4();
+
+  const initialState = {
+    slideUrl: slideUrl,
+    currentPage: 1,
+    annotations: [],
+    feedbacks: [],
+  };
+
+  roomStore.set(roomId, initialState);
+
+  res.status(201).json({ roomId });
+});
+
+export default roomRouter;
