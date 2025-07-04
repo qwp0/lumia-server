b174a612ecb22d201fd89aa77edd16a23cc90525
Soyoung Park <soyoung008@naver.com>
2025-07-04 15:46:58 +0900

    feat: Server 인스턴스 생성 및 CORS 설정 포함한 기본 설정 추가 #5

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
+import http from "http";
+import { Server } from "socket.io";
+
 import "./firebase/firebaseAdmin.js";
 import slideRouter from "./routes/slides.js";
 import roomRouter from "./routes/room.js";
@@ -10,6 +13,23 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("소켓 연결: ", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("연결 해제:", socket.id);
+  });
+});
 
 app.use(cors());
 app.use(express.json());
@@ -20,6 +40,6 @@ app.get("/", (req, res) => {
   res.send("Hello from Lumia Server!");
 });
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
